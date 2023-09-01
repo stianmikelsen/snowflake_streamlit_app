c2a54106c2547e229555755efa895e58f6808950
Stian Mikelsen <37293403+stianmikelsen@users.noreply.github.com>
2023-09-01 14:39:12 +0200

    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as s
 
 s.title("Snowflake App")
+
+s.header('Breakfast Menu')
+s.text('Omega 3 & Blueberry Oatmeal')
+s.text('Kale, Spinach & Rocket Smoothie')
+s.text('Hard-Boiled Free-Range Egg')
