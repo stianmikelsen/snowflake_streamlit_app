63727b4cbff2579f69dc64c650dc3e56797773c5
Stian Mikelsen <37293403+stianmikelsen@users.noreply.github.com>
2023-09-01 14:41:26 +0200

    added menu items with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as s
 s.title("Snowflake App")
 
 s.header('Breakfast Menu')
-s.text('Omega 3 & Blueberry Oatmeal')
-s.text('Kale, Spinach & Rocket Smoothie')
-s.text('Hard-Boiled Free-Range Egg')
+s.text('🥣 Omega 3 & Blueberry Oatmeal')
+s.text('🥗 Kale, Spinach & Rocket Smoothie')
+s.text('🐔 Hard-Boiled Free-Range Egg')
+s.text('🥑🍞 Avocado toast')
+
+s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
