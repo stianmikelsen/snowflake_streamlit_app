076cb59d0c97c84b22c3b30404dcab0c4f45173d
Stian Mikelsen <37293403+stianmikelsen@users.noreply.github.com>
2023-09-01 14:45:42 +0200

    added pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as s
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 s.title("Snowflake App")
 
@@ -9,3 +13,4 @@ s.text('🐔 Hard-Boiled Free-Range Egg')
 s.text('🥑🍞 Avocado toast')
 
 s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+s.dataframe(my_fruit_list)
