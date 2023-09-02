3adde878bbb5e22fb1480f1a86c8863eaa637399
Stian Mikelsen <37293403+stianmikelsen@users.noreply.github.com>
2023-09-02 18:06:20 +0200

    added fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as s
 import pandas
+import requests
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -19,3 +21,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 s.dataframe(fruits_to_show)
+
+s.header('Fruityvice advice')
+fruit_choice = s.text_input('What fruit would you like information about?','Kiwi')
+s.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+# s.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+s.dataframe(fruityvice_normalized)
