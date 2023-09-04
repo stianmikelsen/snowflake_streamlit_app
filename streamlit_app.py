575ef9d62faf7d01a3ac5227a723da82cd3245ee
Stian Mikelsen <37293403+stianmikelsen@users.noreply.github.com>
2023-09-04 16:30:22 +0200

    add snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,11 @@ fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_cho
 # s.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 s.dataframe(fruityvice_normalized)
+
+
+my_cnx = snowflake.connector.connect(**s.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+s.text("Hello from Snowflake:")
+s.text(my_data_row)
